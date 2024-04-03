850526f1ea14a3b2a41e3af4791b5512deabc7c4
Ali <dev.alisevinc@gmail.com>
2024-04-03 22:52:00 +0300

    small update

--- src/components/geolocation-demo/GeoLoc.tsx
+++ src/components/geolocation-demo/GeoLoc.tsx
@@ -30,12 +30,12 @@ export default function GeoLoc() {
         setError(null);
         setPosition({ lat, lng });
         const res = await fetch(
-          `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}`
+          `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}`,
         );
         const data = await res.json();
         console.log(data);
         const address = {
-          city: data.address.city,
+          city: data.address.city || data.address.province,
           country: data.address.country,
           region: data.address.region,
           road: data.address.road,
@@ -45,18 +45,18 @@ export default function GeoLoc() {
       },
       (err) => {
         setError(err.message || "Getting position failed.");
-      }
+      },
     );
   }
 
   return (
-    <div className="w-96 bg-stone-200 mx-auto mt-24 rounded-xl p-8 flex flex-col gap-8 items-center">
-      <h1 className="text-2xl font-semibold text-center">
+    <div className="mx-auto mt-24 flex w-96 flex-col items-center gap-8 rounded-xl bg-stone-200 p-8">
+      <h1 className="text-center text-2xl font-semibold">
         Get Your Position&Address
       </h1>
       <button
         onClick={handleGetPosition}
-        className=" bg-stone-600 text-stone-50 px-4 py-2 rounded-lg"
+        className=" rounded-lg bg-stone-600 px-4 py-2 text-stone-50"
       >
         Get Position
       </button>
@@ -88,6 +88,15 @@ export default function GeoLoc() {
           <p>
             <span>Road: </span> <b>{address.road || "---"}</b>
           </p>
+          {address.city && (
+            <a
+              target="_blank"
+              href={`https://en.wikipedia.org/wiki/${address.city}`}
+              className="text-blue-500 hover:underline"
+            >
+              Wikipedia/{address.city}
+            </a>
+          )}
         </div>
       )}
       {error && <p className="text-red-600">{error}</p>}
